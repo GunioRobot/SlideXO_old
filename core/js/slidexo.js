40fabf2f98d70797b1b71858a8f01ead5f7eb31b
Jason Daihl <jason@daihl.com>
2010-07-08 16:08:17 -0500

    Updated CSS, JS navigation

--- core/js/slidexo.js
+++ core/js/slidexo.js
@@ -1,40 +1,75 @@
+var SLIDE_COUNT;
+var SLIDE_INDEX;
+
 window.onload = function() {
-	//init();
+	init();
 };
 
 document.onkeydown = function(e) {
-	//alert(e.keyCode);
 	keyboard(e.keyCode);
 }
 
-function init() {
-	var root = document.documentElement;
-	
-	root.className = "slideshow";
-	/*if(hasClass(root,"outline")) {
-		removeClass(root,"outline");
-		addClass(root,"slideshow");
-	}*/
+window.onhashchange = function(e) {
+	SLIDE_INDEX = parseInt(location.hash.split("-")[1]);
+	//alert(SLIDE_INDEX);
 }
 
+function init() {
+	/* Assign slide IDs */
+	var slides = document.getElementsByClassName("xo-slide");
+	SLIDE_COUNT = slides.length;
+	for (var i=0; i < SLIDE_COUNT; i++) {
+		slides[i].setAttribute("id","slide-" + (i+1));
+	}
+	
+	/* Cue up first slide */
+	location.hash = "slide-1";
+	SLIDE_INDEX = 1;
+	//alert(window.history.length);
+}
+
+/*+ Delegates keyboard inputs */
 function keyboard(key) {
 	switch(key) {
-		case 83:
+		case 83:	/* s */
 			mode_switch();
+			break;
+		case 10: 	// return
+		case 13: 	// enter
+		case 32: 	// spacebar
+		case 34: 	// page down
+		case 39: 	// rightkey
+		case 40: 	// downkey			
+			if (SLIDE_INDEX < SLIDE_COUNT) {
+				location.hash = "slide-" + (SLIDE_INDEX + 1);
+			}
+			//alert("Next Slide: slide-" + (SLIDE_INDEX + 1));
+			break;
+		case 8:		// backspace/delete
+		case 33: 	// page up
+		case 37: 	// leftkey
+		case 38: 	// upkey
+			if (SLIDE_INDEX != 1) {
+				location.hash = "slide-" + (SLIDE_INDEX - 1);
+			}
+			//alert("Previous Slide: " + location.hash);
 			break;
 	}
 }
 
+/*+ Switches between Outline and Slideshow modes */
 function mode_switch() {
 	var root = document.documentElement;
-	if(root.className === "outline") {
-		root.className = "slideshow";
+	if (hasClass(root,"outline")) {
+		removeClass(root,"outline");
+		addClass(root,"slideshow");
 	} else {
-		root.className = "outline";
+		removeClass(root,"slideshow");
+		addClass(root,"outline");
 	}
 }
 
-/* Utility functions */
+/*+ Utility */
 function hasClass(ele,cls) {
 	return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
 }
